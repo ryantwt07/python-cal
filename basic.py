d59216709b1e391ae942942cec3b60b0186f671e
Ryan Thum <thum_wai_tat_ryan@students.edu.sg>
2021-07-23 00:33:26 +0000

    Initial commit

---
+++ basic.py
@@ -0,0 +1,23 @@
+def addition():
+  num1 = float(input("Enter 1st Number: "))
+  num2 = float(input("Enter 2nd Number: "))
+  result = num1 + num2
+  print(result)
+
+def subtraction():
+  num1 = float(input("Enter 1st Number: "))
+  num2 = float(input("Enter 2nd Number: "))
+  result = num1 - num2
+  print(result)
+
+def multiplication():
+  num1 = float(input("Enter 1st Number: "))
+  num2 = float(input("Enter 2nd Number: "))
+  result = num1 * num2
+  print(result)
+
+def division():
+  num1 = float(input("Enter 1st Number: "))
+  num2 = float(input("Enter 2nd Number: "))
+  result = num1 / num2
+  print(result)
